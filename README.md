3ed3f35d82c2c37492fbdf582ff11cd7259e549f
Zoffix Znet <cpan@zoffix.com>
2014-01-31 21:52:45 -0500

    Prerelease

---
+++ README.md
@@ -0,0 +1,31 @@
+# NAME
+
+App::ZofCMS::PluginBundle::Naughty - a collection of ZofCMS plugins that are troublesome to install
+
+# DESCRIPTION
+
+See documentation for individual modules in this distribution.
+
+# REPOSITORY
+
+Fork this module on GitHub:
+[https://github.com/zoffixznet/App-ZofCMS-PluginBundle-Naughty](https://github.com/zoffixznet/App-ZofCMS-PluginBundle-Naughty)
+
+# BUGS
+
+To report bugs or request features, please use
+[https://github.com/zoffixznet/App-ZofCMS-PluginBundle-Naughty/issues](https://github.com/zoffixznet/App-ZofCMS-PluginBundle-Naughty/issues)
+
+If you can't access GitHub, you can email your request
+to `bug-App-ZofCMS-PluginBundle-Naughty at rt.cpan.org`
+
+# AUTHOR
+
+Zoffix Znet <zoffix at cpan.org>
+([http://zoffix.com/](http://zoffix.com/), [http://haslayout.net/](http://haslayout.net/))
+
+# LICENSE
+
+You can use and distribute this module under the same terms as Perl itself.
+See the `LICENSE` file included in this distribution for complete
+details.
---
+++ lib/App/ZofCMS/PluginBundle/Naughty.pm
@@ -0,0 +1,49 @@
+package App::ZofCMS::PluginBundle::Naughty;
+
+use strict;
+use warnings;
+
+# VERSION
+
+q|
+Programming is 10% science, 20% ingenuity, and 70% getting the
+ingenuity to work with the science
+|;
+
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+App::ZofCMS::PluginBundle::Naughty - a collection of ZofCMS plugins that are troublesome to install
+
+=head1 DESCRIPTION
+
+See documentation for individual modules in this distribution.
+
+=head1 REPOSITORY
+
+Fork this module on GitHub:
+L<https://github.com/zoffixznet/App-ZofCMS-PluginBundle-Naughty>
+
+=head1 BUGS
+
+To report bugs or request features, please use
+L<https://github.com/zoffixznet/App-ZofCMS-PluginBundle-Naughty/issues>
+
+If you can't access GitHub, you can email your request
+to C<bug-App-ZofCMS-PluginBundle-Naughty at rt.cpan.org>
+
+=head1 AUTHOR
+
+Zoffix Znet <zoffix at cpan.org>
+(L<http://zoffix.com/>, L<http://haslayout.net/>)
+
+=head1 LICENSE
+
+You can use and distribute this module under the same terms as Perl itself.
+See the C<LICENSE> file included in this distribution for complete
+details.
+
+=cut
